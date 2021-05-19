d0da9698673d2936fa74dac51f91e0839fe40bd2
Daniel Ockiya <annonbytes@gmail.com>
2021-05-19 22:49:15 +0100

    Login component setup with routing

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Login from './components/login.jsx'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
-       <h1>Hello Bigbytes of code</h1>
+    <Router>
+        <Switch>
+            <Route exact path="/">
+                <Login />
+            </Route>
+        </Switch>
+    </Router>
     </div>
   );
 }
