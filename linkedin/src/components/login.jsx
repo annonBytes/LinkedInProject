0951ffa8c5fc7a45af6a6a6f141b93997eb7afd4
Daniel Ockiya <annonbytes@gmail.com>
2021-05-21 23:36:50 +0100

    Login boilerplate

--- linkedin/src/components/login.jsx
+++ linkedin/src/components/login.jsx
@@ -3,9 +3,25 @@ import styled from "styled-components"
 const Login = () => {
     return (
         <div className="login">
-            Login component
+            <Container>
+                <Nav>
+                    <a href="/">
+                        <img src="/images/linkedin.svg" alt="logo" />
+                    </a>
+                </Nav>
+            </Container>
         </div>
     )
 }
 
+const Container = styled.div `
+
+`
+
+const Nav = styled.div `
+
+`
+
 export default Login
+
+
