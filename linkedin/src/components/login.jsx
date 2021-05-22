5c4556851dbb782da6edb6ff9c8209a88614079a
Daniel Ockiya <annonbytes@gmail.com>
2021-05-22 22:42:11 +0100

    Login Styling Added

--- linkedin/src/components/login.jsx
+++ linkedin/src/components/login.jsx
@@ -14,12 +14,24 @@ const Login = () => {
     )
 }
 
-const Container = styled.div `
-
+const Container = styled.div
+    `
+padding: 0px;
 `
 
 const Nav = styled.div `
-
+  max-width: 1128px;
+  margin: auto;
+  padding: 12px 0 16px;
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: space-between;
+  position: relative
+  
+  & > a {
+  width: 135px;
+  }
 `
 
 export default Login
